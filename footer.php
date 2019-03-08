1339bf7c214037e36c49694a781878b0622f9cb9
zhurof <41473707+zhurof@users.noreply.github.com>
2019-03-08 20:23:06 +0300

    without modal and responsive styles

--- footer.php
+++ footer.php
@@ -1,6 +1,27 @@
 	<footer class="footer">
 		<div class="wrapper footer__wrapper">
-      
+      <div class="footer__main">
+        <a href="#/" class="logo logo_white footer__logo"></a>
+        <p><strong>Юридический адрес</strong></p>
+        <p>109156, Москва, улица Авиаконструктора Миля, дом 3, помещение 1, комната 5</p>
+        <p><strong>Офис</strong></p>
+        <p>125009, Москва, Бизнес-центр Берта Хаус, Старопименовский пер., 18</p>
+      </div>
+      <div class="footer__nav">
+        <p>Нужны дополнительные услуги? Закажите вместе с арендой 1С</p>
+        <ul>
+          <li><a href="#">Программы и лицензии</a></li>
+          <li><a href="#">Телефония для сайта</a></li>
+          <li><a href="#">Бухгалтерский аудит и сопровождение</a></li>
+        </ul>
+      </div>
+      <div class="footer__contacts">
+        <p>Контактные телефоны</p>
+        <div class="footer__phone"><a href="tel: +74957778358">+7 495 777 83 58</a></div>
+        <div class="footer__phone"><a href="tel: +78007757562">+7 800 775 75 62</a> <small>(бесплатно по РФ)</small></div>
+        <a href="#">Пользовательское соглашение</a><br>
+        <a href="#">Политика конфиденциальности</a>
+      </div>
     </div>
 	</footer>
 	<script src="//code.jquery.com/jquery-latest.min.js"></script>
--- js/script.js
+++ js/script.js
@@ -1,4 +1,58 @@
-﻿$('.dropdown__link').click(function(e){
+﻿//шапка
+function headerStatus(){
+  if($(window).scrollTop() > 0){
+    $('.header').addClass('header_small');
+  }else{
+    $('.header_small').removeClass('header_small');
+  }
+}
+$(window).on('load scroll',headerStatus);
+//Модальные окна
+function openModal(modalId, initiator){  
+  var scrollWidth = window.innerWidth - document.body.clientWidth;//Ширина полосы прокрутки
+  
+	$('.modal-wrapper').children().unwrap();
+	if(!$('#'+modalId).length){
+		alert('Ошибка вызова модального окна');
+		return false;
+	}
+	$('#'+modalId).trigger('beforeShow',initiator).wrap('<div class="modal-wrapper" style="display:none" />');
+	$('.modal-wrapper').fadeIn(400,function(){
+    $('#'+modalId).trigger('afterShow',initiator);
+  });
+	
+	if(scrollWidth){
+		$('html').css('padding-right',scrollWidth);
+		$('body').css('overflow-y','hidden');
+	}
+}
+function closeModal(){
+	$('.modal-wrapper').fadeOut(200, function(){
+		$('html').css('padding-right','');
+		$('body').css('overflow-y','');
+	});
+}
+$(document).on('click', '[data-modal]', function(e){
+	e.preventDefault();
+	var modal = $(this).data('modal');
+	openModal(modal,e.target);
+})
+$(document).on('click', '.modal__close', closeModal);
+
+$(document).on('mousedown', '.modal-wrapper', function(e){
+	if(!$('.modal').is(e.target) && $('.modal').has(e.target).length === 0){
+		closeModal();
+	}
+})
+$(document).keydown(function(e){
+	//Закрытие окна на Esc
+	if(e.which == 27){
+		closeModal();
+	}
+});
+
+//Всякое выпадающее барахло
+$('.dropdown__link').click(function(e){
   e.preventDefault();
   $(this).toggleClass('active');
   $(this).siblings('.dropdown__content').slideToggle(200);
@@ -8,6 +62,7 @@ $('.hidden-text>a').click(function(e){
   $(this).toggleClass('active');
   $(this).siblings('.hidden-text__content').toggleClass('open');
 })
+//Слайдеры
 $('.reviews__slider').slick({
   arrows:false,
   asNavFor:'.reviews__pic'
